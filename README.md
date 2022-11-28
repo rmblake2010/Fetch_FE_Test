137a46c249407899d77974c6e53b3966861fc460
Ryan Blake <xbungalowx@gmail.com>
2022-11-28 12:33:46 -0600

    began form and added dependencies

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # Fetch_FE_Test
+
+
+### Overview
+This is a take home test application for Fetch Rewards.
+
+The goal of this application is to create a form that asks the following 
+
+| Form |   |   |   |   |
+|---|---|---|---|---|
+| Full Name  |  User-generated 
+|  Email |  User-generated 
+| Password  | User-generated  
+|  Occupation | API-Generated    
+| State  |  API-Generated    
+
+
+- The form will require all options be filled in before submission is possible.
+- The form body will be sent back to given API. 
+- JSON Data is named:
+{
+    "name": "???",
+    "email": "???",
+    "password": "???",
+    "occupation": "???",
+    "state": "???"
+}
+
+## Vite.js + React
+-  Using vite.js for scaffolding
+- Vite is much more lightweight than CRA 
+- initiated w/ ``` npm create vite@latest react-form-app --template react ```
+
+## MUI
+- Using Material UI for styling
+- An easy to use framework that is great for prototyping ideas or styling entire sites with!
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,57 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { Button, Paper, TextField, Typography, Select, InputLabel, MenuItem, Menu, FormControl } from '@mui/material'
 import './App.css'
 
 function App() {
-  return(
-    <div>
-      <h1>Hello World!</h1>
-    </div>
-  )
+//API Data
+const [data, setData] = useState([])
+//Form Data
+let [postData, setPostData] = useState({ name: '', email: '', password: '', occupation: '', state: ''}) 
+
+
+
+useEffect(() => {
+  const fetchData = async () => {
+    let response = await fetch('https://frontend-take-home.fetchrewards.com/form')
+    let resData = await response.json()
+    console.log(resData)
+    setData(resData)
+  }
+  fetchData()
+}, [])
+
+const menuDropdown = (data) => {
+  if(data){
+    data.occupations.map((occupation, i) => {
+      console.log(i)
+      return(
+        <MenuItem key={i}>{occupation}</MenuItem>
+      )
+    })
+  }
+}
+
+
+
+return(
+  <Paper>
+    <FormControl autoComplete='off'>
+      <Typography variant='h6'>Sign up</Typography>
+      <Typography variant='h7'>Please fill out all boxes</Typography>
+      <TextField required name='name' variant='outlined' label='Full Name' fullWidth/>
+      <TextField required name='email' variant='outlined' label='Email' fullWidth/>
+      <TextField required name='password' variant='outlined' label='Password' fullWidth/>   
+      <InputLabel>Occupation</InputLabel>   
+      <Select
+          defaultValue={""}      
+          label="Occupation"
+        >
+          {menuDropdown(data)}
+        </Select>
+
+    </FormControl>
+  </Paper>
+)
 }
 
 export default App
